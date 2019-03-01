bba1957e6883e010ca81b5898caa898e5183e1ee
michaellikaiye <mye1@stuy.edu>
2019-02-28 21:56:11 -0500

    done

--- main.py
+++ main.py
@@ -2,39 +2,41 @@ from display import *
 from draw import *
 from parser import *
 from matrix import *
-print("Testing MATRICES")
-print("Testing m2 = new_matrix(4,2)")
-m2 = []
-print("Testing add edge (1, 2, 3), (4, 5, 6)")
-add_edge(m2, 1, 2, 3, 4, 5, 6)
-print_matrix(m2)
-print("Testing identity matrix m0")
-m0 = new_matrix()
-ident(m0)
-print_matrix(m0)
-print("m1 =")
-m1 = []
-add_edge(m1, 1, 2, 3, 4, 5, 6)
-add_edge(m1, 7, 8, 9, 10, 11, 12)
-print_matrix(m1)
-print("Testing multiplication m1 * m2 =")
-matrix_mult(m1, m2)
-print_matrix(m2)
-print("Testing add point (1, 2, 3)")
-add_point(m2, 1, 2, 3)
-print_matrix(m2)
+# print("Testing MATRICES")
+# print("Testing m2 = new_matrix(4,2)")
+# m2 = []
+# print("Testing add edge (1, 2, 3), (4, 5, 6)")
+# add_edge(m2, 1, 2, 3, 4, 5, 6)
+# print_matrix(m2)
+# print("Testing identity matrix m0")
+# m0 = new_matrix()
+# ident(m0)
+# print_matrix(m0)
+# print("m1 =")
+# m1 = []
+# add_edge(m1, 1, 2, 3, 4, 5, 6)
+# add_edge(m1, 7, 8, 9, 10, 11, 12)
+# print_matrix(m1)
+# print("Testing multiplication m1 * m2 =")
+# matrix_mult(m1, m2)
+# print_matrix(m2)
+# print("Testing add point (1, 2, 3)")
+# add_point(m2, 1, 2, 3)
+# print_matrix(m2)
+
+# s = make_scale(1,2,3)
+# t = make_translate(4,5,6)
+#
+# print_matrix(s)
+# print_matrix(t)
+# s = [[2, 2, 2, 1]]
+# matrix_mult(t, s)
+#
+# print_matrix(s)
+#s = make_rotZ(60)
 screen = new_screen()
 color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
-s = make_scale(1,2,3)
-t = make_translate(4,5,6)
-
-print_matrix(s)
-print_matrix(t)
-s = [[2, 2, 2, 1]]
-matrix_mult(t, s)
-
-print_matrix(s)
-#s = make_rotZ(60)
 parse_file( 'script', edges, transform, screen, color )
+#parse_csv('Drawing2.csv', edges, transform, screen, color )
--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 from display import *
 from matrix import *
 from draw import *
-
+import math
 """
 Goes through the file named filename and performs all of the actions listed in that file.
 The file follows the following format:
@@ -31,13 +31,84 @@ The file follows the following format:
          quit: end parsing
 
 See the file script for an example of the file format
+0   1       2       3       4           5       6       7               8   9   10      11      12      13
+Count,Name,Center X,Center Y,Center Z,Radius,Start Angle,Total Angle,End X,End Y,End Z,Start X,Start Y,Start Z
+1,Line,,,,,,,207.0863,155.4836,1.8669,207.0863,155.4836,0.8914
+1,Arc,207.0863,142.3145,1.8669,2.4387,180,90,,,,,,
 """
+def add_arc(edge, radius, start, total, x, y, z):
+    if z != 0.8914:
+        points = []
+        n = 6
+        deg = start
+        for i in range(n):
+            inc = total/(n - 1)
+            theta = math.radians(deg)
+            ax = radius * math.cos(theta)
+            ay = radius * math.sin(theta)
+            incp = [x + ax, y + ay, z]
+            points.append(incp)
+            deg += inc
+        for j in range(len(points) - 1):
+            add_edge(edge, points[j][0],points[j][1], points[j][2], points[j+1][0], points[j+1][1], points[j+1][2])
+        points = []
+        n = 6
+        deg = start
+        for i in range(n):
+            inc = total/(n - 1)
+            theta = math.radians(deg)
+            ax = radius * math.cos(theta)
+            ay = radius * math.sin(theta)
+            incp = [x + ax, y + ay, 0.8914]
+            points.append(incp)
+            deg += inc
+        for j in range(len(points) - 1):
+            add_edge(edge, points[j][0],points[j][1], points[j][2], points[j+1][0], points[j+1][1], points[j+1][2])
+
+def parse_csv(fname, points, transform, screen, color):
+    f = open(fname, 'r')
+    lines = f.read().split('\n')
+    n = 0
+    while n < len(lines) - 1:
+        words = lines[n].split(',')
+        print(len(lines), n + 1)
+        if words[1] == 'Line':
+            ints = [float(words[8+x]) for x in range(6)]
+            add_edge(points, ints[3], ints[4], ints[5], ints[0], ints[1], ints[2])
+        if words[1] == 'Arc':
+            ints = [float(words[2+x]) for x in range(6)]
+            add_arc(points, ints[3], ints[4], ints[5], ints[0], ints[1], ints[2])
+        n += 1
+    ident(transform)
+    x = make_rotX(30)
+    matrix_mult(x, transform)
+    y = make_rotY(30)
+    matrix_mult(y, transform)
+    z = make_rotX(30)
+    matrix_mult(z, transform)
+    matrix_mult(transform, points)
+    clear_screen(screen)
+    # draw_lines(points, screen, color)
+    # display(screen)
+    #
+
+
+    ident(transform)
+    t = make_translate(-100, -80, -100)
+    matrix_mult(t, transform)
+    s = make_scale(5, 5, 5)
+    matrix_mult(s, transform)
+    matrix_mult(transform, points)
+    clear_screen(screen)
+    draw_lines(points, screen, color)
+    display(screen)
+    save_extension(screen, 'key')
+
 def parse_file( fname, points, transform, screen, color ):
     f = open(fname, 'r')
     lines = f.read().split('\n')
     n = 0
     while n < len(lines):
-        print(n+1)
         action = lines[n]
         if action == 'line':
             ints = [int(x) for x in lines[n+1].split(' ')]
@@ -89,5 +160,3 @@ def parse_file( fname, points, transform, screen, color ):
             n = len(lines)
         else:
             n += 1
-
-        print_matrix(points)
